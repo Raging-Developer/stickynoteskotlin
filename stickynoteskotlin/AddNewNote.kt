3ee0af5664d5a4f08e647b09a6312efcd4f299ef
Chris Harte <raging.development@gmail.com>
2025-08-11 13:44:51 +0100

    Update AddNewNote.kt
    Use the viewModel

--- stickynoteskotlin/AddNewNote.kt
+++ stickynoteskotlin/AddNewNote.kt
@@ -1,40 +1,46 @@
 package app.stickynoteskotlin
 
-
 import android.app.AlertDialog
 import android.content.ContentValues
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Typeface
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-
 import app.stickynoteskotlin.Db_Helper.Companion.DB_TABLE
 import app.stickynoteskotlin.Db_Helper.Companion.key_note
 import app.stickynoteskotlin.Db_Helper.Companion.key_title
-import app.stickynoteskotlin.MainActivity.Companion.font_size
 import app.stickynoteskotlin.ui.theme.StickyNotesKotlinTheme
 import app.stickynoteskotlin.ui.theme.addScreen
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.getValue
 
 class AddNewNote(): AppCompatActivity() {
+    private val viewModel: NotesViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         setContent{
+            val fontSize by viewModel.fontSize.collectAsState()
+            val fontName by viewModel.fontName.collectAsState()
+
             StickyNotesKotlinTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    addScreen()
+                    addScreen(fontName, fontSize)
                 }
             }
         }
@@ -53,7 +59,7 @@ fun addNewNote(
 
     var dia = AlertDialog.Builder(context)
     val db = Db_Helper(context).writableDatabase
-    val date_format = DateTimeFormatter.ofPattern(" dd/MM/yyyy HH:mm:ss")
+    val date_format = DateTimeFormatter.ofPattern(" dd-MMMM-yyyy HH:mm:ss")
     val current = LocalDateTime.now().format(date_format)
     var title = "$current"
 
@@ -76,7 +82,7 @@ fun addNewNote(
             dia.show()
         } else {
             dia.setTitle("Success")
-            dia.setMessage("Course has been added")
+            dia.setMessage("Note has been added")
             dia.setPositiveButton("Okie Dokie", { l: DialogInterface, i: Int ->
                 context.startActivity(Intent(context, MainActivity::class.java)) })
             dia.create()
