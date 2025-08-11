67b93a2ba1ea5dfe34cf3e6f3fa346aefa98ea20
Chris Harte <raging.development@gmail.com>
2025-08-11 13:35:21 +0100

    Update MainActivity.kt
    Added an interface and started using a viewModel

--- stickynoteskotlin/MainActivity.kt
+++ stickynoteskotlin/MainActivity.kt
@@ -1,51 +1,35 @@
 package app.stickynoteskotlin
 
-
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
 import app.stickynoteskotlin.ui.theme.StickyNotesKotlinTheme
-import kotlin.jvm.java
+import kotlin.getValue
 
-class MainActivity : AppCompatActivity() {
+interface StickyNoteActions{
+    fun addNoteScreen()
+    fun updateNoteScreen(noteId: Long, noteTitle: String, noteText: String)
+    fun deleteNoteScreen(noteId: Long, noteText: String)
+}
 
-    companion object {
-        var font_size: Int? = 20
-        var font_name: String = ""
-    }
+class MainActivity : AppCompatActivity(), StickyNoteActions {
+    private val viewModel: NotesViewModel by viewModels()
 
-    override fun onResume() {
-        super.onResume()
-        font_size = get_font_size(this)
-        font_name = get_font_name(this)
-        call_readDataFromDatabase()
-    }
-
-    override fun onPause(){
-        super.onPause()
-        font_size = get_font_size(this)
-        font_name = get_font_name(this)
-        call_readDataFromDatabase()
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        font_size = get_font_size(this)
-        font_name = get_font_name(this)
 
         setContentView(R.layout.mainactivity)
         setSupportActionBar(findViewById(R.id.my_toolbar))
@@ -70,12 +54,50 @@ class MainActivity : AppCompatActivity() {
                         color = MaterialTheme.colorScheme.background
                     ) {
                         Scaffold {
-                            val compose_view = ComposeView(this)
-                            stickyNoteView(LocalContext.current, compose_view)
+                            // Observe data from ViewModel
+                            val notes by viewModel.notes.collectAsState()
+                            val fontSize by viewModel.fontSize.collectAsState()
+                            val fontName by viewModel.fontName.collectAsState()
+
+                            // Pass the observed state to your Composable
+                            stickyNoteView(notes, fontSize, fontName, this)
                         }
                     }
                 }
             }
+    }
+
+    override fun addNoteScreen() {
+            val i = Intent(this, AddNewNote::class.java)
+            startActivity(i)
+    }
+
+    override fun updateNoteScreen(noteId: Long,
+                                  noteTitle: String,
+                                  noteText: String)
+    {
+        val i = Intent(this, UpdateNote::class.java)
+        i.putExtra("description", noteText)
+        i.putExtra("id", noteId)
+        startActivity(i)
+    }
+
+    override fun deleteNoteScreen(noteId: Long, noteText: String) {
+        val i = Intent(this, DeleteNote::class.java)
+        i.putExtra("note", noteText)
+        i.putExtra("id", noteId)
+        startActivity(i)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        viewModel.loadFontSettings()
+        viewModel.loadNotes()
+        call_readDataFromDatabase()
+    }
+
+    override fun onPause(){
+        super.onPause()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -89,14 +111,16 @@ class MainActivity : AppCompatActivity() {
             R.id.aboutBox -> {
                 val a = Intent(this, About::class.java)
                 a.putExtra("title", "A note taking app")
-                a.putExtra("text", "A very simple note taking app.\n"
-                        + "After adding, short click to edit,\n"
-                        + "long click to get delete option.\n")
+                a.putExtra("text", " A very simple note taking app.\n"
+                        + " After adding, short click to edit,\n"
+                        + " long click to get delete option.\n")
                 startActivity(a)
+                return true
             }
             R.id.action_settings -> {
                 val i = Intent(this, Prefs::class.java)
                 startActivity(i)
+                return true
             }
             R.id.credit -> {
                 val i = Intent(this, Credit::class.java)
@@ -104,11 +128,16 @@ class MainActivity : AppCompatActivity() {
                 i.putExtra("text", " Made by Chris Harte. Because he could\n"
                 + " The fonts are Note_this and IndieFlower taken from fontsquirrel.\n")
                 startActivity(i)
+                return true
             }
             else -> {
-                super.onOptionsItemSelected(item)
+               return super.onOptionsItemSelected(item)
             }
         }
         return false
     }
 }
+
+
+
+
